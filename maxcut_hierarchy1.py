aac3ab38828dc3a33f0d6fc27f45eb5e77683db2
Nathan <nleung100@gmail.com>
2023-07-21 21:45:54 +0100

    Added comments and minor changes

--- maxcut_hierarchy1.py
+++ maxcut_hierarchy1.py
@@ -1,17 +1,26 @@
 import cvxpy as cp
 import numpy as np
+import time
 
-n = 10
+start = time.time()
+
+# Setting up initial data of problem
+n = 100
 
 L = np.random.randn(n,n)
+L = L + L.T
+
 
 Y = cp.Variable((n,n), symmetric=True)
 
-objective = cp.Maximize(cp.trace(L @ Y))
+# Constructing diagonal constraints on matrix (y_emptyset = 1)
 constraints = []
 constraints.append(Y >> 0)
 for i in range(n):
     constraints.append(Y[i][i] == 1)
+
+# Solving and processing
+objective = cp.Maximize(cp.trace(L @ Y))
 
 problem  = cp.Problem(objective, constraints)
 
@@ -20,4 +29,4 @@ problem.solve()
 print('The optimal value is', problem.value)
 print('An optimal Y is')
 print(Y.value)
-
+print('Solve time for Mosek is', time.time() - start, 's')
--- maxcut_hierarchy2.py
+++ maxcut_hierarchy2.py
@@ -1,25 +1,33 @@
 import cvxpy as cp
 import numpy as np
 import math
+import time
 
-n = 4
+start = time.time()
+
+# Setting up initial data of problem
+n = 15
 
 L = np.random.randn(n,n)
+L = L + L.T
 
 dim = 1 + math.comb(n,2)
 
 Y = cp.Variable((dim,dim), symmetric=True)
 
+# Constructing diaognal constraints on matrix (y_emptyset = 1)
 constraints = []
 constraints.append(Y >> 0)
 for i in range(dim):
     constraints.append(Y[i][i] == 1)
 
+# Using degree 2 monomials to index matrix
 monomials = [[]]
 for i in range(1,n+1):
     for j in range(i+1,n+1):
         monomials.append([i,j])
 
+# Constructing symmetric difference constraints on matrix entries (y_symdiff(U,V))
 fours = {}
 for i in range(dim):
     for j in range(i+1,dim):
@@ -29,23 +37,34 @@ for i in range(dim):
             b = temp[1]
             constraints.append(Y[i][j] == Y[0][int((a - 1)*n - a*(a-1)/2 + b - a)])
         if len(temp) == 4:
-            if tuple(temp) in fours.keys():
+            if tuple(temp) in fours:
                 constraints.append(Y[i][j] == Y[fours[tuple(temp)][0]][fours[tuple(temp)][1]])
             else:
                 fours.update({tuple(temp): (i,j)})
 
-
+# Constructing objective function
 sum = 0
-for i in range(n):
-    for j in range(n):
-        sum += Y[0][int((i - 1)*n - i*(i-1)/2 + j - i)] * L[i][j]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        ri = min(i,j)
+        rj = max(i,j)
 
+        if i == j:
+            sum += L[i-1][j-1]
+        else:
+            sum += Y[0][int((ri - 1)*n - ri*(ri-1)/2 + rj - ri)] * L[i-1][j-1]
+
+print('Initialisation took', time.time() - start, 's')
+
+# Solving and processing
 objective = cp.Maximize(sum)
 
-problem  = cp.Problem(objective, constraints)
+problem1  = cp.Problem(objective, constraints)
 
-problem.solve()
+problem1.solve(solver=cp.SCS, verbose=True)
 
-print('The optimal value is', problem.value)
+
+print('The optimal value is', problem1.value)
 print('An optimal Y is')
 print(Y.value)
+print('Solve time for SCS is', time.time() - start, 's')
